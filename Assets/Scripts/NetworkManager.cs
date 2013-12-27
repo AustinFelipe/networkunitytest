80880cbfeee9a7a284863e0d03618534d79a8c15
AustinFelipe <austinfelipe@ig.com.br>
2013-12-27 15:37:37 -0300

    Init commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public Transform playerPrefab;
+
+	private string ipToConnect = "";
+	private int portToConnect = 5000;
+
+	private GUISkin skin;
+	private GameManager gm;
+
+	void Start()
+	{
+		ipToConnect = "192.168.0.22";
+		gm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameManager> ();
+		skin = gm.mainSkin; 
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer(2, portToConnect, !Network.HavePublicAddress());
+	}
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 200, 50), "Iniciar Server"))
+				StartServer();
+
+			ipToConnect = GUI.TextField(new Rect(100, 200, 200, 20), ipToConnect);
+
+			if (GUI.Button(new Rect(100, 220, 200, 50), "Conectar Server"))
+				JoinServer();
+		}
+	}
+	
+	private void JoinServer()
+	{
+		Network.Connect(ipToConnect, portToConnect);
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+		SpawnPlayer();
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
+		SpawnPlayer();
+	}
+	
+	private void SpawnPlayer()
+	{
+		Vector3 initialPosition = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(20, 0, 0)).x, 0);
+
+		if (!Network.isServer)
+		{
+			initialPosition.x = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - 20, 0, 0)).x;
+		}
+
+		Network.Instantiate(playerPrefab, initialPosition, transform.rotation, 0);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2f;
+	
+	void Update()
+	{
+		if (networkView.isMine)
+			InputMovement();
+	}
+	
+	void InputMovement()
+	{
+		transform.Translate(0f, 
+		                    Input.GetAxis ("Vertical") * Time.deltaTime * speed,
+		                    0f);
+	}
+}
